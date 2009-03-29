71639e307ae1ce162b6f1ab21bd3e6e044717078
Lionel Armanet <lionel.armanet@gmail.com>
2009-03-29 19:53:02 +0000

    Adding new fusion theme and base theme
    svn path=/branches/1.0-alpha/; revision=28

--- src/main/java/org/odlabs/wiquery/ui/droppable/DroppableAjaxBehavior.java
+++ src/main/java/org/odlabs/wiquery/ui/droppable/DroppableAjaxBehavior.java
@@ -2,11 +2,20 @@ package org.odlabs.wiquery.ui.droppable;
 
 import org.apache.wicket.ajax.AbstractDefaultAjaxBehavior;
 import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.odlabs.wiquery.core.javascript.JsScope;
 
 
 public abstract class DroppableAjaxBehavior extends AbstractDefaultAjaxBehavior {
 
 	private static final long serialVersionUID = 1L;
+
+	private DroppableBehavior droppableBehavior;
+	
+	@Override
+	protected void onBind() {
+		getComponent().add(droppableBehavior = new DroppableBehavior());
+		droppableBehavior.setOnDrop(JsScope.quickScope(this.getCallbackScript()));
+	}
 
 	@Override
 	protected void respond(AjaxRequestTarget target) {
