00c0515333675acda3c47d0cfa5fa4547a34d5dd
Lionel Armanet <lionel.armanet@gmail.com>
2009-05-27 22:53:49 +0000

    Modification of onDrop callbalck signature to get the dropped component
    svn path=/branches/1.0-alpha/; revision=76

--- src/main/java/org/odlabs/wiquery/ui/droppable/DroppableAjaxBehavior.java
+++ src/main/java/org/odlabs/wiquery/ui/droppable/DroppableAjaxBehavior.java
@@ -1,5 +1,8 @@
 package org.odlabs.wiquery.ui.droppable;
 
+import org.apache.wicket.Component;
+import org.apache.wicket.MarkupContainer;
+import org.apache.wicket.Component.IVisitor;
 import org.apache.wicket.ajax.AbstractDefaultAjaxBehavior;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.odlabs.wiquery.core.javascript.JsScope;
@@ -19,10 +22,11 @@ public abstract class DroppableAjaxBehavior extends AbstractDefaultAjaxBehavior 
 		
 			@Override
 			protected void execute(JsScopeContext scopeContext) {
-				scopeContext.append(getCallbackScript());
+				scopeContext.append("wicketAjaxGet('" + getCallbackUrl(true) +
+						"&droppedId='+ui.draggable[0].id, null,null, function() {return true;})");
 			}
 		
-		}); 
+		});	
 	}
 
 	public void setAccept(String accept) {
@@ -34,6 +38,38 @@ public abstract class DroppableAjaxBehavior extends AbstractDefaultAjaxBehavior 
 		onDrop(target);
 	}
 	
-	public abstract void onDrop(AjaxRequestTarget target);
+	public final void onDrop(AjaxRequestTarget target) {
+		// getting dropped element id to retreive the wicket component
+		String input = this.getComponent().getRequest().getParameter("droppedId");
+		MarkupIdVisitor visitor = new MarkupIdVisitor(input);
+		this.getComponent().getPage().visitChildren(visitor);
+		onDrop(visitor.getFoundComponent(), target);
+	}
+	
+	public abstract void onDrop(Component droppedComponent, AjaxRequestTarget ajaxRequestTarget);
 
+	// TODO refactor -> create separated file
+	private static class MarkupIdVisitor implements IVisitor<Component> {
+		private final String id;
+		private Component found;
+
+		public MarkupIdVisitor(String id) {
+			this.id = id;
+		}
+
+		public Object component(Component component) {
+			if (component.getMarkupId().equals(id)) {
+				this.found = component;
+				return IVisitor.STOP_TRAVERSAL;
+			}
+			if (component instanceof MarkupContainer) {
+				return ((MarkupContainer)component).visitChildren(this);
+			}
+			return IVisitor.CONTINUE_TRAVERSAL;
+		}
+		
+		public Component getFoundComponent() {
+			return found;
+		}
+	}
 }
