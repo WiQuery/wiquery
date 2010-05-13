b572e723e485ba6d0374421e25af827c268093cc
Julien Roche <roche.jul@gmail.com>
2010-05-13 12:22:53 +0000

    1.0.2 branch & 1.1 XP branch : - change the WiQueryInstantiationListener. Find a better way to retrieve the current instance
    svn path=/branches/1.0.2/; revision=333

--- src/main/java/org/odlabs/wiquery/core/commons/WiQueryInstantiationListener.java
+++ src/main/java/org/odlabs/wiquery/core/commons/WiQueryInstantiationListener.java
@@ -55,7 +55,7 @@ public class WiQueryInstantiationListener implements
 	private static final long serialVersionUID = -7398777039788778234L;
 	
 	/** Singleton of {@link WiQueryInstantiationListener} object. */
-	private static WiQueryInstantiationListener current;
+	private static WiQueryInstantiationListener current = null;
 	
 	/** Mutex */
 	private static Object mutex = new Object();
@@ -85,14 +85,6 @@ public class WiQueryInstantiationListener implements
 	public WiQueryInstantiationListener() {
 		super();
 		
-		synchronized (mutex) {
-			if(current != null){
-				throw new WicketRuntimeException("A WiQueryInstantiationListener is already instanciate");
-			}
-			
-			current = this;
-		}
-		
 		// try to read some options
 		Application app = Application.get();
 		
@@ -115,6 +107,10 @@ public class WiQueryInstantiationListener implements
 		} else {
 			autoImportJQueryResource = true;
 			listeners = new ArrayList<WiQueryPluginRenderingListener>();
+		}
+		
+		synchronized (mutex) {
+			current = this;
 		}
 	}
 
