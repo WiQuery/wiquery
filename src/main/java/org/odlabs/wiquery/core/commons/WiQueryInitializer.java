d9054f329ef1dd3a776932970a24d557c2f65ceb
Julien Roche <roche.jul@gmail.com>
2010-11-21 20:50:37 +0000

    Wiquery 1.2
    -> issue 123: base for an external wiquery initializer
    
    svn path=/trunk/; revision=563

--- src/main/java/org/odlabs/wiquery/core/commons/WiQueryInitializer.java
+++ src/main/java/org/odlabs/wiquery/core/commons/WiQueryInitializer.java
@@ -21,9 +21,19 @@
  */
 package org.odlabs.wiquery.core.commons;
 
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Properties;
+
 import org.apache.wicket.Application;
 import org.apache.wicket.IInitializer;
 import org.apache.wicket.MetaDataKey;
+import org.apache.wicket.util.lang.Objects;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * $Id$
@@ -43,14 +53,85 @@ public class WiQueryInitializer implements IInitializer {
 		private static final long serialVersionUID = 1L;
 	};
 	
+	/** Logger */
+	private static final Logger LOGGER = LoggerFactory.getLogger(WiQueryInitializer.class);
+	
 	/**
 	 * {@inheritDoc}
 	 * @see org.apache.wicket.IInitializer#init(org.apache.wicket.Application)
 	 */
 	public void init(Application application) {
 		application.addPostComponentOnBeforeRenderListener(new WiQueryRenderingListener());
+		
 		//check for IWiQuerySettings on the application
 		WiQuerySettings settings = application instanceof IWiQuerySettings ? ((IWiQuerySettings) application).getWiQuerySettings() : null;
 		application.setMetaData(WIQUERY_INSTANCE_KEY, settings == null ? new WiQuerySettings() : settings);
+	
+		// IWiQueryInitializer treatments
+		retrieveAndCallInitializers(application, settings);
+	}
+	
+	private IWiQueryInitializer getIWiQueryInitializer(String className) {
+		if(className == null){
+			return null;
+		}
+		
+		return (IWiQueryInitializer) Objects.newInstance(className);
+	}
+	
+	/**
+	 * Method finding and calling the {@link IWiQueryInitializer}
+	 */
+	private void retrieveAndCallInitializers(Application application, WiQuerySettings wiQuerySettings) {
+		InputStream in = null;
+		Iterator<URL> resources = null;
+		IWiQueryInitializer wiQueryInitializer;
+		List<IWiQueryInitializer> initializers = new ArrayList<IWiQueryInitializer>();
+		Properties properties = null;
+		URL url = null;
+		
+		try	{
+			resources = application.getApplicationSettings().getClassResolver().getResources("wiquery.properties");
+			while (resources.hasNext()) {
+				try	{
+					url = resources.next();
+					LOGGER.info("Find the wiQuery initializer:", url.toString());
+					
+					properties = new Properties();
+					in = url.openStream();
+					properties.load(in);
+					
+					wiQueryInitializer = getIWiQueryInitializer(
+							properties.getProperty("initializer"));
+					
+					if(wiQueryInitializer != null){
+						initializers.add(wiQueryInitializer);
+					}
+					
+					wiQueryInitializer = getIWiQueryInitializer(
+							properties.getProperty(application.getName() + "-initializer"));
+					
+					if(wiQueryInitializer != null){
+						initializers.add(wiQueryInitializer);
+					}
+					
+				}
+				finally {
+					if (in != null) {
+						in.close();
+					}
+				}
+			}
+		}
+		catch (Exception e) {
+			LOGGER.error("Unable to load initializers file", e);
+		}
+		
+		if(initializers.size() > 0){
+			for(IWiQueryInitializer initializer : initializers){
+				LOGGER.info("[" + application.getName() + "] init: " + initializer);
+				initializer.init(application, wiQuerySettings);
+			}
+		}
 	}
 }
