ec9c988ebbe2080f20d56c9eefca60a3c9b2e047
Hielke Hoeve <hielke.hoeve@gmail.com>
2011-08-24 08:01:18 +0000

    make wantOnSelectionChangedNotifications() overridable so the ButtonCheckSet is ajax enabled.
    svn path=/branches/wicket-1.5/; revision=1162

--- wiquery-jquery-ui/src/main/java/org/odlabs/wiquery/ui/button/ButtonCheckSet.java
+++ wiquery-jquery-ui/src/main/java/org/odlabs/wiquery/ui/button/ButtonCheckSet.java
@@ -45,108 +45,151 @@ import org.odlabs.wiquery.ui.widget.WidgetJavaScriptResourceReference;
 /**
  * $Id$
  * <p>
- * Creates a set of button checkbox UI component from this {@link Panel}'s
- * HTML markup.
+ * Creates a set of button checkbox UI component from this {@link Panel}'s HTML markup.
  * </p>
  * 
- * @param <T> The model object type
+ * @param <T>
+ *            The model object type
  * 
  * @author Julien Roche
  * @since 1.1
  */
 @WiQueryUIPlugin
-public class ButtonCheckSet<T extends Serializable> extends Panel implements IWiQueryPlugin {
+public class ButtonCheckSet<T extends Serializable> extends Panel implements IWiQueryPlugin
+{
 	// Constants
-	/**	Constant of serialization */
+	/** Constant of serialization */
 	private static final long serialVersionUID = -7837076246968244110L;
-	
+
 	// Properties
 	private final CheckGroup<T> checkGroup;
-	
+
 	/**
 	 * Constructor
-	 * @param id Wicket identifiant
-	 * @param checks List of checks
+	 * 
+	 * @param id
+	 *            Wicket identifiant
+	 * @param checks
+	 *            List of checks
 	 */
-	public ButtonCheckSet(String id, IModel<List<? extends ButtonElement<T>>> checks) {
+	public ButtonCheckSet(String id, IModel<List< ? extends ButtonElement<T>>> checks)
+	{
 		this(id, checks, new CollectionModel<T>(new ArrayList<T>()));
 	}
 
 	/**
 	 * Constructor
-	 * @param id Wicket identifiant
-	 * @param checks List of checks
-	 * @param model Model of the default object
+	 * 
+	 * @param id
+	 *            Wicket identifiant
+	 * @param checks
+	 *            List of checks
+	 * @param model
+	 *            Model of the default object
 	 */
-	public ButtonCheckSet(String id, IModel<List<? extends ButtonElement<T>>> checks,
-			IModel<Collection<T>> model) {
+	public ButtonCheckSet(String id, IModel<List< ? extends ButtonElement<T>>> checks,
+			IModel<Collection<T>> model)
+	{
 		super(id);
-		
-		checkGroup = new CheckGroup<T>("buttonCheckSetGroup", model){
+
+		checkGroup = new CheckGroup<T>("buttonCheckSetGroup", model)
+		{
 			private static final long serialVersionUID = 8265281439115476364L;
 
 			/**
 			 * {@inheritDoc}
+			 * 
 			 * @see org.apache.wicket.markup.html.form.CheckGroup#onSelectionChanged(java.util.Collection)
 			 */
 			@Override
-			protected void onSelectionChanged(final Collection<? extends T> newSelection) {
+			protected void onSelectionChanged(final Collection< ? extends T> newSelection)
+			{
 				ButtonCheckSet.this.onSelectionChanged(newSelection);
+			}
+
+			/**
+			 * This method should be overridden to return true if it is desirable to have
+			 * on-selection-changed notification.
+			 * 
+			 * @return true if component should receive on-selection-changed
+			 *         notifications, false otherwise
+			 */
+			@Override
+			protected boolean wantOnSelectionChangedNotifications()
+			{
+				return ButtonCheckSet.this.wantOnSelectionChangedNotifications();
 			}
 		};
 		checkGroup.setOutputMarkupId(true);
 		checkGroup.setRenderBodyOnly(false);
 		add(checkGroup);
-		
-		ListView<? extends ButtonElement<T>> view = new ListView<ButtonElement<T>>("buttonCheckSetView", checks) {
-			private static final long serialVersionUID = 1L;
 
-			/**
-			 * {@inheritDoc}
-			 * @see org.apache.wicket.markup.html.list.ListView#populateItem(org.apache.wicket.markup.html.list.ListItem)
-			 */
-			@Override
-			protected void populateItem(ListItem<ButtonElement<T>> item) {
-				ButtonElement<T> buttonElement = item.getModelObject();
-				
-				Check<T> check = newCheck("buttonCheck", buttonElement.getModel(), checkGroup);
-				check.setLabel(buttonElement.getLabel());
-				check.setOutputMarkupId(true);
-				
-				SimpleFormComponentLabel label = new SimpleFormComponentLabel("buttonCheckLabel", check);
-				
-				item.add(check);
-				item.add(label);
-			}
-		};
+		ListView< ? extends ButtonElement<T>> view =
+			new ListView<ButtonElement<T>>("buttonCheckSetView", checks)
+			{
+				private static final long serialVersionUID = 1L;
+
+				/**
+				 * {@inheritDoc}
+				 * 
+				 * @see org.apache.wicket.markup.html.list.ListView#populateItem(org.apache.wicket.markup.html.list.ListItem)
+				 */
+				@Override
+				protected void populateItem(ListItem<ButtonElement<T>> item)
+				{
+					ButtonElement<T> buttonElement = item.getModelObject();
+
+					Check<T> check = newCheck("buttonCheck", buttonElement.getModel(), checkGroup);
+					check.setLabel(buttonElement.getLabel());
+					check.setOutputMarkupId(true);
+
+					SimpleFormComponentLabel label =
+						new SimpleFormComponentLabel("buttonCheckLabel", check);
+
+					item.add(check);
+					item.add(label);
+				}
+			};
 		checkGroup.add(view);
 	}
-	
+
 	/**
 	 * Constructor
-	 * @param id Wicket identifiant
-	 * @param radios List of checks
+	 * 
+	 * @param id
+	 *            Wicket identifiant
+	 * @param radios
+	 *            List of checks
 	 */
-	public ButtonCheckSet(String id, List<? extends ButtonElement<T>> checks) {
+	public ButtonCheckSet(String id, List< ? extends ButtonElement<T>> checks)
+	{
 		this(id, Model.ofList(checks));
 	}
-	
+
 	/**
 	 * Constructor
-	 * @param id Wicket identifiant
-	 * @param radios List of checks
-	 * @param model Model of the default object
+	 * 
+	 * @param id
+	 *            Wicket identifiant
+	 * @param radios
+	 *            List of checks
+	 * @param model
+	 *            Model of the default object
 	 */
-	public ButtonCheckSet(String id, List<? extends ButtonElement<T>> checks, 
-			IModel<Collection<T>> model) {
+	public ButtonCheckSet(String id, List< ? extends ButtonElement<T>> checks,
+			IModel<Collection<T>> model)
+	{
 		this(id, Model.ofList(checks), model);
 	}
-	
+
 	/**
 	 * {@inheritDoc}
+	 * 
 	 * @see org.odlabs.wiquery.core.IWiQueryPlugin#contribute(org.odlabs.wiquery.core.commons.WiQueryResourceManager)
 	 */
-	public void renderHead(IHeaderResponse response) {
+	@Override
+	public void renderHead(IHeaderResponse response)
+	{
 		response.renderJavaScriptReference(WidgetJavaScriptResourceReference.get());
 		response.renderJavaScriptReference(ButtonJavaScriptResourceReference.get());
 	}
@@ -154,42 +197,65 @@ public class ButtonCheckSet<T extends Serializable> extends Panel implements IWi
 	/**
 	 * @return the check group of this component
 	 */
-	public CheckGroup<T> getCheckGroup() {
+	public CheckGroup<T> getCheckGroup()
+	{
 		return checkGroup;
 	}
-	
+
 	/**
 	 * Method creating a new {@link Check}
-	 * @param wicketId Wicket identifiant
-	 * @param model Model to use
-	 * @param group Group of the {@link Check}
+	 * 
+	 * @param wicketId
+	 *            Wicket identifiant
+	 * @param model
+	 *            Model to use
+	 * @param group
+	 *            Group of the {@link Check}
 	 * @return a {@link Check}
 	 */
-	protected Check<T> newCheck(String wicketId, IModel<T> model, CheckGroup<T> group) {
+	protected Check<T> newCheck(String wicketId, IModel<T> model, CheckGroup<T> group)
+	{
 		Check<T> check = new Check<T>(wicketId, model, group);
 		return check;
 	}
-	
+
 	/**
-	 * Template method that can be overridden by clients that implement IOnChangeListener to be
-	 * notified by onChange events of a select element. This method does nothing by default.
+	 * Template method that can be overridden by clients that implement IOnChangeListener
+	 * to be notified by onChange events of a select element. This method does nothing by
+	 * default.
 	 * <p>
-	 * Called when a {@link Check} is clicked in a {@link CheckGroup} that wants to be notified of
-	 * this event. This method is to be implemented by clients that want to be notified of selection
-	 * events.
+	 * Called when a {@link Check} is clicked in a {@link CheckGroup} that wants to be
+	 * notified of this event. This method is to be implemented by clients that want to be
+	 * notified of selection events.
 	 * 
 	 * @param newSelection
-	 *            The new selection of the {@link CheckGroup}. NOTE this is the same as you would
-	 *            get by calling getModelObject() if the new selection were current
+	 *            The new selection of the {@link CheckGroup}. NOTE this is the same as
+	 *            you would get by calling getModelObject() if the new selection were
+	 *            current
 	 */
-	protected void onSelectionChanged(final Collection<? extends T> newSelection){
+	protected void onSelectionChanged(final Collection< ? extends T> newSelection)
+	{
+	}
+
+	/**
+	 * This method should be overridden to return true if it is desirable to have
+	 * on-selection-changed notification.
+	 * 
+	 * @return true if component should receive on-selection-changed notifications, false
+	 *         otherwise
+	 */
+	protected boolean wantOnSelectionChangedNotifications()
+	{
+		return false;
 	}
 
 	/**
 	 * {@inheritDoc}
+	 * 
 	 * @see org.odlabs.wiquery.core.IWiQueryPlugin#statement()
 	 */
-	public JsStatement statement() {
+	public JsStatement statement()
+	{
 		return new JsQuery(checkGroup).$().chain("buttonset");
 	}
 }
