afd0a6f7d91ed11ae647e86a9a3ca17eb17dfcb5
Hielke Hoeve <hielke.hoeve@gmail.com>
2011-12-07 20:45:27 +0000

    upgrade to jquery 1.6.4, 1.7 has a lot of core changes which i have not had a chance to test yet.
    svn path=/trunk/; revision=1953

--- wiquery-core/src/main/java/org/odlabs/wiquery/core/resources/CoreJavaScriptResourceReference.java
+++ wiquery-core/src/main/java/org/odlabs/wiquery/core/resources/CoreJavaScriptResourceReference.java
@@ -53,7 +53,7 @@ public class CoreJavaScriptResourceReference extends WiQueryJavaScriptResourceRe
 	 */
 	private CoreJavaScriptResourceReference()
 	{
-		super(CoreJavaScriptResourceReference.class, "jquery-1.6.3.js");
+		super(CoreJavaScriptResourceReference.class, "jquery-1.6.4.js");
 	}
 
 }
