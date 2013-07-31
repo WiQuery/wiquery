e49ea9c2d65414cda7df41f80e9f7e8e566bd89e
boschman <sverre.boschman@topicus.nl>
2013-07-31 16:47:54 +0200

    JQuery Tooltip support

---
+++ wiquery-jquery-ui/src/main/java/org/odlabs/wiquery/ui/tooltip/TooltipJavaScriptResourceReference.java
@@ -0,0 +1,42 @@
+package org.odlabs.wiquery.ui.tooltip;
+
+import org.apache.wicket.markup.head.HeaderItem;
+import org.apache.wicket.request.resource.JavaScriptResourceReference;
+import org.odlabs.wiquery.core.resources.JavaScriptHeaderItems;
+import org.odlabs.wiquery.ui.core.CoreUIJavaScriptResourceReference;
+import org.odlabs.wiquery.ui.position.PositionJavaScriptResourceReference;
+import org.odlabs.wiquery.ui.widget.WidgetJavaScriptResourceReference;
+
+public class TooltipJavaScriptResourceReference extends JavaScriptResourceReference
+{
+	private static final long serialVersionUID = -4771815414204892357L;
+
+	/**
+	 * Singleton instance.
+	 */
+	private static TooltipJavaScriptResourceReference instance =
+		new TooltipJavaScriptResourceReference();
+
+	/**
+	 * Builds a new instance of {@link TooltipJavaScriptResourceReference}.
+	 */
+	private TooltipJavaScriptResourceReference()
+	{
+		super(TooltipJavaScriptResourceReference.class, "jquery.ui.tooltip.js");
+	}
+
+	/**
+	 * Returns the {@link TooltipJavaScriptResourceReference} instance.
+	 */
+	public static TooltipJavaScriptResourceReference get()
+	{
+		return instance;
+	}
+
+	@Override
+	public Iterable< ? extends HeaderItem> getDependencies()
+	{
+		return JavaScriptHeaderItems.forReferences(CoreUIJavaScriptResourceReference.get(),
+			WidgetJavaScriptResourceReference.get(), PositionJavaScriptResourceReference.get());
+	}
+}
