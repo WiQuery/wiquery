ca2afaabf1a7542004fd1db23673218b2da12807
Hielke Hoeve <hielke.hoeve@gmail.com>
2013-12-17 11:55:14 +0100

    jetty to a version with only javax servlet 2.5

--- wiquery-demo/src/test/java/org/odlabs/wiquery/Start.java
+++ wiquery-demo/src/test/java/org/odlabs/wiquery/Start.java
@@ -1,6 +1,7 @@
 package org.odlabs.wiquery;
 
 import org.eclipse.jetty.server.Connector;
+
 import org.eclipse.jetty.server.HttpConnectionFactory;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
